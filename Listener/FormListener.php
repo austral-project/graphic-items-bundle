3d2ccc7ab5d517a45ff53780a499c0f4f861c4b5
Matthieu Beurel <matthieu@mbeurel.com>
2023-05-26 17:59:30 +0200

    feat : add graphic items field and listener form

---
+++ Listener/FormListener.php
@@ -0,0 +1,64 @@
+<?php
+/*
+ * This file is part of the Austral GraphicItems Bundle package.
+ *
+ * (c) Austral <support@austral.dev>
+ *
+ * For the full copyright and license information, please view the LICENSE
+ * file that was distributed with this source code.
+ */
+
+namespace Austral\GraphicItemsBundle\Listener;
+
+use Austral\FormBundle\Event\FormEvent;
+use Austral\FormBundle\Field\Base\FieldInterface;
+use Austral\GraphicItemsBundle\Field\GraphicItemField;
+
+/**
+ * Austral FormListener Listener.
+ * @author Matthieu Beurel <matthieu@austral.dev>
+ * @final
+ */
+class FormListener
+{
+
+  /**
+   * @param FormEvent $formEvent
+   * @param string|null $type
+   *
+   * @throws \Exception
+   */
+  public function formAddAutoFields(FormEvent $formEvent, ?string $type = null)
+  {
+    /** @var FieldInterface $field */
+    foreach($formEvent->getFormMapper()->allFields() as $field)
+    {
+      if($field instanceof GraphicItemField)
+      {
+        $options = $field->getOptions();
+        $options["attr"]["autocomplete"] = "off";
+        $options["attr"]["data-popin-update-input"] = "field-{$field->getFieldname()}";
+        $field->setPopinId("popup-graphic-items-{$field->getFieldname()}")
+          ->setOptions($options);
+        $formEvent->getFormMapper()->addPopin("popup-graphic-items-{$field->getFieldname()}", $field->getFieldname(), array(
+          "button"  =>  array(
+            "entitled"      =>  "",
+            "picto"         =>  "austral-picto-link",
+            "class"         =>  "button-picto",
+            "data"          =>  array(
+              "data-check-value"  =>  json_encode(array(
+                "*[data-popin-update-input='{field-{$field->getFieldname()}}']"
+              ))
+            )
+          ),
+          "popin"  =>  array(
+            "template"      =>  "graphicItems",
+          )
+        ));
+      }
+    }
+
+  }
+
+
+}
